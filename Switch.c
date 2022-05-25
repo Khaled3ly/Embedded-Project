5a43bcf8fc92830d2063a2fad77afdaa3e413d68
Khaled3ly <105653804+Khaled3ly@users.noreply.github.com>
2022-05-26 00:33:09 +0200

    Update Switch.c

--- Switch.c
+++ Switch.c
@@ -2,8 +2,9 @@
 
 void Switch_Init (unsigned char port_name, unsigned short int pin_number){
 	Set_Pin_DIR (port_name, pin_number,0);
+	Enable_PU(port_name,pin_number);
 }
 
-unsigned short int Switch_Read (unsigned char port_name, unsigned short int pin_number){
+unsigned char Switch_Read (unsigned char port_name, unsigned short int pin_number){
 	return Read_Pin (port_name, pin_number);
 }
