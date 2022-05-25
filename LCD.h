5438c34851d1aec576fc97c1acf179a614995053
Omarezz123 <105800539+Omarezz123@users.noreply.github.com>
2022-05-26 00:36:09 +0200

    Update LCD.h

--- LCD.h
+++ LCD.h
@@ -1,20 +1,22 @@
 #ifndef LCD_H_  
 #define LCD_H_
 #include "Drivers.h"
+#include "tm4c123gh6pm.h"
 #include <stdio.h> 
-#define Data_Port 'A'   // Add data ports here
-#define CTRL_Port 'C'	// Add control ports here
-#define E 4 			// CTRL E pin
-#define RS 5 			// CTRL RegisterSelect (Command or Data) pin
-#define RW 6 			// CTRL ReadWrite pin
+#define Data_Port 'B'   // Add data ports here
+#define CTRL_Port 'A'	// Add control ports here
+#define RS 5 			// CTRL ReadWrite pin
+#define RW 6 			// CTRL RegisterSelect (Command or Data) pin
+#define E 7 			// CTRL E pin
 
 void LCD_Init (void);
-void pulse (void);
-void LCD_Send_CMD (char);
-void LCD_Send_Chr (char);
-void LCD_Send_String (char);
+void LCD_Send_CMD (unsigned long);
+void LCD_Send_Chr (unsigned long);
+void LCD_Send_String (char *);
 void LCD_Send_Int(int);
-void LCD_CLR_Screen(void);
-void LCD_Move_Cursor (char, char);
+//void LCD_CLR_Screen(void);
+void LCD_Move_Cursor (unsigned long, unsigned long);
+void delay_ms(int);
+void delay_us(int);
 
 #endif
