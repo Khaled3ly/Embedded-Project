408c724157b67722523afd68c49c4fdcd96bf821
Omarezz123 <105800539+Omarezz123@users.noreply.github.com>
2022-05-26 00:35:49 +0200

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -2,51 +2,48 @@
 
 
 
-void pulse (void){
-	Write_Pin(CTRL_Port,E,1);
-	delay (2);
-	Write_Pin(CTRL_Port,E,0);
+void LCD_Send_CMD(unsigned long cmd){
+	GPIO_PORTB_DATA_R = cmd;
+	GPIO_PORTA_DATA_R &= ~0x20;
+	GPIO_PORTA_DATA_R |= 0x80;
+	delay_us(40);
+	GPIO_PORTA_DATA_R &= ~0x80;
 }
 
-void LCD_Send_CMD (char cmd){
-	Write_Port(Data_Port,cmd);
-	Write_Pin(CTRL_Port,RS,0);
-	pulse();
-	delay (1);
+void LCD_Send_Chr(unsigned long data){
+	GPIO_PORTB_DATA_R = data;
+	GPIO_PORTA_DATA_R |= 0x20;
+	GPIO_PORTA_DATA_R |= 0x80;
+	delay_us(40);
+	GPIO_PORTA_DATA_R &= ~0x80;
+	delay_ms(10);
 }
 
-void LCD_Send_Chr (char chr){
-	Write_Port(Data_Port,chr);
-	Write_Pin(CTRL_Port,RS,1);
-	pulse();
-	delay (1);
+void LCD_Send_String (char *data){
+	while ((*data) != '\0'){
+		LCD_Send_Chr((*data));
+		data++;
+	}
 }
 
-//void LCD_Send_String (char *data){
-//	while ((*data) != '0'){
-//		LCD_Send_Chr((*data));
-//		data++;
-//	}
+void LCD_Send_Int(int number)
+{
+	int i = 0;
+  char toprint[4] = {0};
+  sprintf(toprint, "%d", number);
+  while(toprint[i] != '\0')
+  {
+    LCD_Send_Chr(toprint[i]);
+    i++;
+  }
+}
+
+//void LCD_CLR_Screen(void){
+//	LCD_Send_CMD(0x01);
+//	delay_ms (10);
 //}
 
-//void LCD_Send_Int(int number)
-//{
-//  char toprint[4] = {0};
-//  sprintf(toprint, "%d", number);
-//  int i = 0;
-//  while(toprint[i] != '\0')
-//  {
-//   LCD_Send_Chr(toprint[i]);
-//    i++;
-// }
-//}
-
-void LCD_CLR_Screen(void){
-	LCD_Send_CMD(0x01);
-	delay (10);
-}
-
-void LCD_Move_Cursor (char row, char col){
+void LCD_Move_Cursor (unsigned long row, unsigned long col){
 	char position = 0;
 	if (row == 1){
 		position = (0x80) + col - 1;
@@ -55,21 +52,32 @@ void LCD_Move_Cursor (char row, char col){
 		position = (0xC0) + col - 1;
 	}
 	LCD_Send_CMD(position);
-	delay (1);
+	delay_ms (1);
+}
+void delay_us(int time)
+{
+int i, j;
+for(i = 0 ; i < time; i++)
+  for(j = 0; j < 3; j++){}
+}
+void delay_ms(int time)
+{
+int i, j;
+for(i = 0 ; i < time; i++)
+  for(j = 0; j < 3180; j++){}
 }
 
 void LCD_Init (void){
-	Port_Init(Data_Port);
-	Port_Init(CTRL_Port);
-	Set_Port_DIR (Data_Port,0xFF);
-	Set_Pin_DIR (CTRL_Port,E,1);
-	Set_Pin_DIR (CTRL_Port,RS,1);
-	Set_Pin_DIR (CTRL_Port,RW,1);
-	Write_Pin (CTRL_Port,RW,0);
-	LCD_Send_CMD(0x38);	// Eight Bits Mode
-	delay (1);
-	LCD_Send_CMD(0x01); // Clear Screen
-	delay (10);
-	LCD_Send_CMD(0x06); // Entry Mode
-	delay (1);
+	Port_Init('A');
+	Port_Init('B');
+	LCD_Send_CMD(0x38); //8 bit
+	delay_ms(2);			
+	LCD_Send_CMD(0x01); // clr
+	delay_ms(2);	
+  LCD_Send_CMD(0x06);
+	delay_ms(2);	
+	LCD_Send_CMD(0x80);
+	delay_ms(2);	
+	LCD_Send_CMD(0x0E);
+	delay_ms(2);	
 }
