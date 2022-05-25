d0c1056b9be32147cee47651cfac21b494b27da7
Khaled3ly <105653804+Khaled3ly@users.noreply.github.com>
2022-05-26 00:32:20 +0200

    Update Keypad.h

--- Keypad.h
+++ Keypad.h
@@ -1,7 +1,8 @@
 #ifndef Keypad_H_  
 #define Keypad_H_
 #include "Drivers.h"
-#define Keypad_Port 'B'
+#define Keypad_Port_Rows 'C'
+#define Keypad_Port_Cols 'E'
 
 void Keypad_Init (void);
 unsigned char Keypad_Read (void);
